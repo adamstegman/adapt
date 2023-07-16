42a9f43af8d5797d6263feacdc3316116427949f
Adam Stegman <me@adamstegman.com>
2023-07-16 16:55:18 -0500

    Make sure 'today' does not start until 4AM

--- app/controllers/behaviors_controller.rb
+++ app/controllers/behaviors_controller.rb
@@ -1,12 +1,18 @@
 class BehaviorsController < ApplicationController
+  NEW_DAY_HOUR = 4
+
   before_action :set_dog
   before_action :set_timezone
 
   def index
     # TODO: sorting
     @behaviors = Behavior.all
-    # FIXME: utility method to determine date if before 4AM
-    @today = Time.current.in_time_zone(@timezone).to_date
+    current_time = Time.current.in_time_zone(@timezone)
+    @today = if current_time.hour < NEW_DAY_HOUR
+      1.day.before(current_time).to_date
+    else
+      current_time.to_date
+    end
     behavior_occurrences_by_behavior_id = @dog.behavior_occurrences.where(seen_on_date: @today).group_by(&:behavior_id)
     @behavior_occurrence_by_behavior_id = @behaviors.each_with_object({}) do |behavior, acc|
       acc[behavior.id] = behavior_occurrences_by_behavior_id[behavior.id]&.[](0) ||
